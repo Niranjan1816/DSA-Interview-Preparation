be2766754d067468a7d47eb93a7a5d390dcce957
NIRANJANA MURTHY H M <niranjanmurthy428@gmail.com>
2026-07-23 23:27:32 +0530

    Implement Floyd's Tortoise and Hare algorithm for duplicate

--- C++/Arrays/LC_0287_Find_Duplicate_Number.cpp
+++ C++/Arrays/LC_0287_Find_Duplicate_Number.cpp
@@ -57,3 +57,32 @@ Key Learnings:
 
 ================================================================================
 */
+
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+
+        int slow = nums[0];
+        int fast = nums[0];
+
+        // Phase 1: Find intersection point
+        do
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        while(slow != fast);
+
+        // Phase 2: Find cycle entrance
+        int slow2 = 0;
+
+        while(slow != slow2)
+        {
+            slow = nums[slow];
+            slow2 = nums[slow2];
+        }
+
+        return slow;
+    }
+};
